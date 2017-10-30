8b5457a3d3b6e595b7177ea1b67d8ecb279004fc
Michał Kopacz <michal.kopacz@rst.com.pl>
2017-10-30 06:50:25 +0100

    remove given

--- test/Domain/Payment/PoolDiscountStrategyTest.php
+++ test/Domain/Payment/PoolDiscountStrategyTest.php
@@ -3,9 +3,7 @@
 namespace RstGroup\ConferenceSystem\Domain\Payment\Test;
 
 use PHPUnit\Framework\TestCase;
-use RstGroup\ConferenceSystem\Domain\Payment\DiscountPoolRepository;
 use RstGroup\ConferenceSystem\Domain\Payment\PoolDiscountStrategy;
-use RstGroup\ConferenceSystem\Domain\Reservation\ConferenceId;
 use RstGroup\ConferenceSystem\Domain\Reservation\Seat;
 
 class PoolDiscountStrategyTest extends TestCase
@@ -15,14 +13,9 @@ class PoolDiscountStrategyTest extends TestCase
      */
     public function returns_discount_equal_to_zero_if_number_of_seats_equals_zero()
     {
-        $numberOfSeats = 0;
-        $conferenceId = new ConferenceId(7);
-        $discountPoolRepository = $this->getMockBuilder(DiscountPoolRepository::class)->getMock();
-        $discountPoolRepository->method('getDiscountPerSeat')->willReturn(50);
-        $poolDiscountStrategy = new PoolDiscountStrategy($conferenceId, $discountPoolRepository);
-        $seat = new Seat("Regular", $numberOfSeats);
+        $poolDiscountStrategy = new PoolDiscountStrategy();
 
-        $discount = $poolDiscountStrategy->calculate($seat);
+        $discount = $poolDiscountStrategy->calculate(new Seat("Regular", 0));
 
         $this->assertSame(0, $discount);
     }
@@ -32,14 +25,7 @@ class PoolDiscountStrategyTest extends TestCase
      */
     public function returns_discount_per_seat_multiplied_by_number_of_seats_when_there_are_enough_discounts()
     {
-        $conferenceId = new ConferenceId(3);
-        $numberOfSeats = 100;
-        $discountPoolRepository = $this->getMockBuilder(DiscountPoolRepository::class)->getMock();
-        $discountPoolRepository->method('getDiscountPerSeat')->willReturn(50);
-        $poolDiscountStrategy = new PoolDiscountStrategy($conferenceId, $discountPoolRepository);
-        $seat = new Seat("Regular", $numberOfSeats);
-
-        $discount = $poolDiscountStrategy->calculate($seat);
+        $discount = $poolDiscountStrategy->calculate(new Seat("Regular", 100));
 
         $this->assertEquals(5000, $discount);
     }
