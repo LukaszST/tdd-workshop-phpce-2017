2c115daf4b85c80fb0aede127982cde1d3e6d49e
Michał Kopacz <michal.kopacz@rst.com.pl>
2017-10-30 06:41:56 +0100

    revert tests

--- test/Domain/Payment/PoolDiscountStrategyTest.php
+++ test/Domain/Payment/PoolDiscountStrategyTest.php
@@ -4,6 +4,7 @@ namespace RstGroup\ConferenceSystem\Domain\Payment\Test;
 
 use PHPUnit\Framework\TestCase;
 use RstGroup\ConferenceSystem\Domain\Payment\PoolDiscountStrategy;
+use RstGroup\ConferenceSystem\Domain\Reservation\ConferenceId;
 use RstGroup\ConferenceSystem\Domain\Reservation\Seat;
 
 class PoolDiscountStrategyTest extends TestCase
@@ -14,7 +15,10 @@ class PoolDiscountStrategyTest extends TestCase
     public function returns_discount_equal_to_zero_if_number_of_seats_equals_zero()
     {
         $numberOfSeats = 0;
-        $poolDiscountStrategy = new PoolDiscountStrategy();
+        $conferenceId = new ConferenceId(7);
+        $discountPoolRepository = $this->getMockBuilder(DiscountPoolRepository::class)->getMock();
+        $discountPoolRepository->method('getDiscountPerSeat')->willReturn(50);
+        $poolDiscountStrategy = new PoolDiscountStrategy($conferenceId, $discountPoolRepository);
         $seat = new Seat("Regular", $numberOfSeats);
 
         $discount = $poolDiscountStrategy->calculate($seat);
@@ -27,6 +31,15 @@ class PoolDiscountStrategyTest extends TestCase
      */
     public function returns_discount_per_seat_multiplied_by_number_of_seats_when_there_are_enough_discounts()
     {
+        $conferenceId = new ConferenceId(3);
+        $numberOfSeats = 100;
+        $discountPoolRepository = $this->getMockBuilder(DiscountPoolRepository::class)->getMock();
+        $discountPoolRepository->method('getDiscountPerSeat')->willReturn(50);
+        $poolDiscountStrategy = new PoolDiscountStrategy($conferenceId, $discountPoolRepository);
+        $seat = new Seat("Regular", $numberOfSeats);
+
+        $discount = $poolDiscountStrategy->calculate($seat);
+
         $this->assertEquals(5000, $discount);
     }
 }
