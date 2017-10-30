c40c8e077af8d7f07072ddfcbf3f195152c8446d
Michał Kopacz <michal.kopacz@rst.com.pl>
2017-10-30 06:57:28 +0100

    revert test

--- test/Domain/Payment/PoolDiscountStrategyTest.php
+++ test/Domain/Payment/PoolDiscountStrategyTest.php
@@ -3,8 +3,6 @@
 namespace RstGroup\ConferenceSystem\Domain\Payment\Test;
 
 use PHPUnit\Framework\TestCase;
-use RstGroup\ConferenceSystem\Domain\Payment\PoolDiscountStrategy;
-use RstGroup\ConferenceSystem\Domain\Reservation\Seat;
 
 class PoolDiscountStrategyTest extends TestCase
 {
@@ -13,10 +11,5 @@ class PoolDiscountStrategyTest extends TestCase
      */
     public function returns_discount_equal_to_zero_if_number_of_seats_equals_zero()
     {
-        $poolDiscountStrategy = new PoolDiscountStrategy();
-
-        $discount = $poolDiscountStrategy->calculate(new Seat("Regular", 0));
-
-        $this->assertSame(0, $discount);
     }
 }
